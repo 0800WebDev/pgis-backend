f3cf8dc32c2b3b39243f400494af51cbfeed2fc1
0800 <spmspy0800@gmail.com>
2026-03-15 12:05:56 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const express=require("express")
+const cors=require("cors")
+const fs=require("fs")
+const path=require("path")
+
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+
+const scriptsDir=path.join(__dirname,"scripts")
+
+if(!fs.existsSync(scriptsDir)){
+fs.mkdirSync(scriptsDir)
+}
+
+app.post("/generate",(req,res)=>{
+
+const html=req.body.html
+const mode=req.body.mode
+
+const id=Math.random().toString(36).substring(2)
+
+const js=`
+(function(){
+
+const mode="${mode}"
+const html=\`${html}\`
+
+if(mode==="replace"){
+document.open()
+document.write(html)
+document.close()
+}else{
+document.body.insertAdjacentHTML("beforeend",html)
+}
+
+})();
+`
+
+fs.writeFileSync(path.join(scriptsDir,id+".js"),js)
+
+res.json({
+url:"https://pgis-backend.onrender.com/scripts/"+id+".js"
+})
+
+})
+
+app.use("/scripts",express.static(scriptsDir))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
